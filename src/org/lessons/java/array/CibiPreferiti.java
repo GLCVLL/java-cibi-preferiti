cb4c9ab7922cac2da6a20bafaebd87fc15c10a7f
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-09 17:47:23 +0100

    added list and top/not top food preferences

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -9,7 +9,20 @@ public class CibiPreferiti {
 	            "Lasagne",
 	            "Gelato",
 	            "Burger",
+	            "Pasta",
 		};
+		
+        // Lunghezza della classifica
+        int lunghezzaClassifica = classificaCibi.length;
+        System.out.println("Lunghezza della classifica: " + lunghezzaClassifica);
+
+        // Il cibo top (prima posizione della classifica)
+        String ciboTop = classificaCibi[0];
+        System.out.println("Il tuo cibo top: " + ciboTop);
+
+        // Il cibo preferito ma non troppo (ultima posizione della classifica)
+        String ciboNonTroppoPreferito = classificaCibi[lunghezzaClassifica - 1];
+        System.out.println("Il tuo cibo preferito ma non troppo: " + ciboNonTroppoPreferito);
 	}
 
 }
