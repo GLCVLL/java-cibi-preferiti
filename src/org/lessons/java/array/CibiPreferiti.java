44e68eae3996c667d8472f7ef7f11c5e899a2429
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-09 17:41:37 +0100

    first commit

---
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -0,0 +1,15 @@
+package org.lessons.java.array;
+
+public class CibiPreferiti {
+	
+	public static void main(String[] args) {
+		String[] classificaCibi ={
+	            "Pizza",
+	            "Sushi",
+	            "Lasagne",
+	            "Gelato",
+	            "Burger",
+		};
+	}
+
+}
