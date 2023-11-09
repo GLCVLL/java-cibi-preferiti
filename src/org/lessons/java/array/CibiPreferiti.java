6a77f3743cf08d5fb97c1396d88eb371cd39bf41
Gioele Iacovella <g.ioele96@outlook.it>
2023-11-09 17:55:04 +0100

    added middle preference

--- src/org/lessons/java/array/CibiPreferiti.java
+++ src/org/lessons/java/array/CibiPreferiti.java
@@ -10,6 +10,7 @@ public class CibiPreferiti {
 	            "Gelato",
 	            "Burger",
 	            "Pasta",
+
 		};
 		
         // Lunghezza della classifica
@@ -23,6 +24,16 @@ public class CibiPreferiti {
         // Il cibo preferito ma non troppo (ultima posizione della classifica)
         String ciboNonTroppoPreferito = classificaCibi[lunghezzaClassifica - 1];
         System.out.println("Il tuo cibo preferito ma non troppo: " + ciboNonTroppoPreferito);
+        
+        // Gestione della posizione mediana
+        if (classificaCibi.length % 2 == 0) {
+            // Se la dimensione dell'array è pari, non c'è una posizione mediana
+            System.out.println("Non c'è una posizione mediana.");
+        } else {
+            // Se la dimensione dell'array è dispari, stampa il cibo in posizione centrale
+            int posizioneMediana = classificaCibi.length / 2;
+            System.out.println("Cibo di mezza classifica: " + classificaCibi[posizioneMediana]);
+        }
 	}
 
 }
